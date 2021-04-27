3eb1f320738e1778402f863ea372e9e0db4e75c5
subi_HQ <ch.subi.p@gmail.com>
2021-04-27 10:17:11 +0900

    Complete after P&Vdc reach the ref. value   in Seq0800

--- CPU_IDE/src/functionBlock/fb_user/fcSeqCommon.c
+++ CPU_IDE/src/functionBlock/fb_user/fcSeqCommon.c
@@ -1467,9 +1467,19 @@ void funcSeq0800(strFC0740Info *fb)
 {
 	if(fb->sysMode.bit.seqDir == SM_SEQDIR_START)
 	{
-		fb->NextStep = Seq0800;
-		fb->seqSts2.bit.seq0800 = SEQ_COMM_COMPLETE;
-		fb->flagSeqComplete = FLAG_SEQ_COMPLETE;
+		if((fb->sysMode.actPwr == SM_ACTPWR_P) & (fb->sysSts.RampPRated == 1) \
+			| (fb->sysMode.actPwr == SM_ACTPWR_VDC) & (fb->sysSts.RampVdcRated == 1))
+		{
+			fb->NextStep = Seq0800;
+			fb->seqSts2.bit.seq0800 = SEQ_COMM_COMPLETE;
+			fb->flagSeqComplete = FLAG_SEQ_COMPLETE;
+		}
+		else
+		{
+			fb->NextStep = Seq0800;
+			fb->seqSts2.bit.seq0800 = SEQ_COMM_PROCESSING;
+			fb->flagSeqComplete = FLAG_SEQ_UNCOMPLETE;
+		}
 	}
 	else if(fb->sysMode.bit.seqDir == SM_SEQDIR_STOP)
 	{
